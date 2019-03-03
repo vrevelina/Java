14e7584fcc2bec512169db08b0c8ec4bdb61425c
vrevelina <35344130+vrevelina@users.noreply.github.com>
2019-03-02 23:09:16 -0800

    added Clock.java
    Methods to help ClockSolver.java

---
+++ ClockSolver/Clock.java
@@ -0,0 +1,163 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Clock.java
+ *  Purpose       :  Provides a class defining methods for the ClockSolver class
+ *  @author       :  Vania Revelina
+ *  Date written  :  2018-03-01
+ *  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
+ *  Notes         :  None right now.  I'll add some as they occur.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+ *  @version 2.0.0  2018-03-01  Vania R.      Modified Clock.java to be a working program
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class Clock {
+  /**
+   *  Class field definintions go here
+   */
+   private static final double DEFAULT_TIME_SLICE_IN_SECONDS = 60.0;
+   private static final double INVALID_ARGUMENT_VALUE = -1.0;
+   private static final double MAXIMUM_DEGREE_VALUE = 360.0;
+   private static final double HOUR_HAND_DEGREES_PER_SECOND = 0.00834;
+   private static final double MINUTE_HAND_DEGREES_PER_SECOND = 0.1;
+   private static double totalSeconds = 0;
+   private static double timeSlice = 0;
+   private static double targetAngle = 0;
+
+  /**
+   *  Constructor goes here
+   */
+   public Clock(double timeSlice) {
+    this.timeSlice = timeSlice;
+   }
+
+  /**
+   *  Methods go here
+   *
+   *  Method to calculate the next tick from the time increment
+   *  @return double-precision value of the current clock tick
+   */
+   public double tick() {
+      totalSeconds += timeSlice;
+      return totalSeconds;
+   }
+
+  /**
+   *  Method to validate the angle argument
+   *  @param   argValue  String from the main programs args[0] input
+   *  @return  double-precision value of the argument
+   *  @throws  NumberFormatException
+   */
+   public static double validateAngleArg( String argValue ) throws NumberFormatException {
+    targetAngle = Double.parseDouble(argValue);
+      if(targetAngle >= 360) {
+        throw new NumberFormatException("enter an angle that is less than 360 degrees");
+      } else if(targetAngle <= 0) {
+        throw new NumberFormatException("enter a positive non-zero real angle");
+      } else {
+        return targetAngle;
+      }
+   }
+
+  /**
+   *  Method to validate the optional time slice argument
+   *  @param  argValue  String from the main programs args[1] input
+   *  @return double-precision value of the argument or -1.0 if invalid
+   *  note: if the main program determines there IS no optional argument supplied,
+   *         I have elected to have it substitute the string "60.0" and call this
+   *         method anyhow.  That makes the main program code more uniform, but
+   *         this is a DESIGN DECISION, not a requirement!
+   *  note: remember that the time slice, if it is small will cause the simulation
+   *         to take a VERY LONG TIME to complete!
+   */
+   public static double validateTimeSliceArg( String argValue ) {
+    timeSlice = Double.parseDouble(argValue);
+      if(timeSlice == 0) {
+        return INVALID_ARGUMENT_VALUE;
+      } else if(timeSlice > 1800) {
+        return INVALID_ARGUMENT_VALUE;
+      } else if(timeSlice < 0) {
+        return INVALID_ARGUMENT_VALUE;
+      } else {
+        return timeSlice;
+      }
+   }
+
+  /**
+   *  Method to calculate and return the current position of the hour hand
+   *  @return double-precision value of the hour hand location
+   */
+   public double getHourHandAngle() {
+      double hourAngle = totalSeconds * HOUR_HAND_DEGREES_PER_SECOND;
+      return hourAngle;
+   }
+
+  /**
+   *  Method to calculate and return the current position of the minute hand
+   *  @return double-precision value of the minute hand location
+   */
+   public double getMinuteHandAngle() {
+      double minuteAngle = totalSeconds * MINUTE_HAND_DEGREES_PER_SECOND;
+      return minuteAngle;
+   }
+
+  /**
+   *  Method to calculate and return the angle between the hands
+   *  @return double-precision value of the angle between the two hands
+   */
+   public double getHandAngle() {
+      double minuteAng = getMinuteHandAngle() % 180;
+      double hourAng = getHourHandAngle() % 180;
+      double handAngle = Math.abs(hourAng - minuteAng);
+      return handAngle;
+   }
+
+  /**
+   *  Method to fetch the total number of seconds
+   *   we can use this to tell when 12 hours have elapsed
+   *  @return double-precision value the total seconds private variable
+   */
+   public double getTotalSeconds() {
+      return totalSeconds;
+   }
+
+  /**
+   *  Method to return a String representation of this clock
+   *  @return String value of the current clock
+   */
+   public String toString() {
+      double hour = Math.floor(totalSeconds/3600);
+      double m = totalSeconds % 3600;
+      double minute = Math.floor(m/60);
+      double second = m % 60;
+      String returnString = "Found target angle of " + targetAngle + " at time: " + 
+                          hour + " hours, " + minute + " minutes, " + second + " seconds";
+      System.out.println(returnString);
+      return returnString;
+   }
+
+  /**
+   *  The main program starts here
+   *  remember the constraints from the project description
+   *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
+   *  be sure to make LOTS of tests!!
+   *  remember you are trying to BREAK your code, not just prove it works!
+   */
+   public static void main( String args[] ) {
+
+      System.out.println( "\nCLOCK CLASS TESTER PROGRAM\n" +
+                          "--------------------------\n" );
+      System.out.println( "  Creating a new clock: " );
+      Clock clock = new Clock(0);
+      System.out.println( "    New clock created: " + clock.toString() );
+      System.out.println( "    Testing validateAngleArg()....");
+      System.out.print( "      sending '  0 degrees', expecting double value   0.0" );
+      try { System.out.println( (0.0 == clock.validateAngleArg( "0.0" )) ? " - got 0.0" : " - no joy" ); }
+      catch( Exception e ) { System.out.println ( " - Exception thrown: " + e.toString() ); }
+   }
+}
