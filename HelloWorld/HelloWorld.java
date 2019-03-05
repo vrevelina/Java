cd2b27571811ec4d31c9c0914fcf849c4e00e013
vrevelina <35344130+vrevelina@users.noreply.github.com>
2019-03-04 21:35:59 -0800

    Hello World!
    Basic java Hello World program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+/* HelloWorld.java
+ * Purpose: Greets the user with an output of "Hello, world!"
+ * Author: Vania Revelina
+ * Date Written: January 11, 2018
+*/
+
+public class HelloWorld {
+	public static void main( String[] args ) {
+	   System.out.println( "Hello, world!" );
+	}
+}
